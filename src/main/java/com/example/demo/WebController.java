3e9bf76d10488e0145b17f0a31d944ed3eef41f0
Abhinav Gupta <Abhinav.Gupta>
2020-04-27 22:35:51 +0530

    first commit

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class WebController {
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping(value = "/")
+	public String home() {
+		return "Okay";
+	}
+	@RequestMapping("/greeting")
+	public String hello() {
+		return "hello";
+	}
+}
